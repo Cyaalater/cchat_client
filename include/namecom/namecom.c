23ddd70c18b2789cc267896b2c938d5b6df5b4b8
mineitan <mineitan@gmail.com>
2022-05-13 09:57:38 +0300

    namecom directory

---
+++ include/namecom/namecom.c
@@ -0,0 +1,5 @@
+//
+// Created by ethan on 13/05/2022.
+//
+#include "namecom.h"
+
---
+++ include/namecom/namecom.h
@@ -0,0 +1,12 @@
+//
+// Created by ethan on 13/05/2022.
+//
+
+#ifndef BUILDDIR_NAMECOM_H
+#define BUILDDIR_NAMECOM_H
+
+int namecom_get_address(char* name);
+
+int namecom_set_address(char* name);
+
+#endif //BUILDDIR_NAMECOM_H
